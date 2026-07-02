0ee5acb7ef412249d7bf9882970328f09ca73dc9
Dionessa <dionessa.biton@gmail.com>
2026-07-02 12:46:08 -0400

    added a test environment for a sequence of scrabble envs

---
+++ gflownet/envs/sequence_scrabble.py
@@ -0,0 +1,38 @@
+"""
+Environment to test sequence scrabble
+"""
+
+from typing import Dict, List, Optional, Tuple, Union
+
+import pandas as pd
+import torch
+from torchtyping import TensorType
+from tqdm import tqdm
+
+from gflownet.envs.composite.sequence import Sequence
+from gflownet.envs.scrabble import Scrabble
+
+from gflownet.utils.common import copy
+
+
+class SequenceScrabble(Sequence):
+    """
+    Environment to test multiple scrabble
+    """
+
+    def __init__(
+        self,
+        **kwargs,
+    ):
+
+        # Initialize list of subenvs:
+        subenvs = [
+            Scrabble(letters=["D","G","O"]),
+            Scrabble(letters=["A", "C", "T"]),
+            Scrabble(letters=["F", "H", "I", "S"])
+        ]
+
+        # Initialize base Stack environment
+        super().__init__(subenvs=tuple(subenvs), **kwargs)
+
+
