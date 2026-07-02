07af47acf0609f3bf1986706bc99e3c959f010c6
Dionessa <dionessa.biton@gmail.com>
2026-07-02 16:54:26 -0400

    added a get_unique_env in the sequence scrabble

--- gflownet/envs/sequence_scrabble.py
+++ gflownet/envs/sequence_scrabble.py
@@ -34,3 +34,15 @@ class SequenceScrabble(Sequence):
 
         # Initialize base Stack environment
         super().__init__(envs_unique=tuple(subenvs), subenvs=tuple(subenvs), **kwargs)
+
+    def _get_unique_environments(self, subenvs):
+        envs_unique = []
+        envs_unique_keys = []
+        unique_indices = []
+        for env in subenvs:
+            env_key = (type(env), tuple(env.action_space), tuple(env.letters))
+            if env_key not in envs_unique_keys:
+                envs_unique_keys.append(env_key)
+                envs_unique.append(env)
+            unique_indices.append(envs_unique_keys.index(env_key))
+        return envs_unique, tuple(envs_unique_keys), unique_indices
