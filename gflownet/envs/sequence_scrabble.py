3e729e0f10b5ccffd2d208a9db32a88222b37155
Dionessa <dionessa.biton@gmail.com>
2026-07-14 10:55:00 -0400

    removed spaces from the readable form of the scrabble subenv

--- gflownet/envs/sequence_scrabble.py
+++ gflownet/envs/sequence_scrabble.py
@@ -83,5 +83,5 @@ class SequenceScrabble(Sequence):
             idx_unique = state["_envs_unique"][key]
             # get the class of the subenv so that we can get the state2readable function
             subenv = self._get_env_unique(idx_unique)
-            body += subenv.state2readable(self._get_substate(state, key))
+            body += str(subenv.state2readable(self._get_substate(state, key))).replace(" ", "")
         return body
