712d79cf40a4413caf9bbffc43aad03b72490c8c
Dionessa <dionessa.biton@gmail.com>
2026-07-21 11:56:59 +0200

    removed print statements used for debugging

--- gflownet/envs/composite/sequence.py
+++ gflownet/envs/composite/sequence.py
@@ -1334,7 +1334,7 @@ class Sequence(CompositeBase):
         input: state dict
         output: list of states
         """
-        print("DEBUGGING:", state)
+        # print("DEBUGGING:", state)
         # here, we will use indices to arrange the possible representations of the state that will correspond to the same sequence
         # representations are used with the condition that the next number can only be inserted in front or at the end
         # so the possible combinations will be 2^n-1 if n=number of elements in the sequence
@@ -1355,7 +1355,7 @@ class Sequence(CompositeBase):
             all_representations = new_representations
         new_state_representations = []
         # then form the state based on the new_representaions
-        print("new representations:", new_representations)
+        # print("new representations:", new_representations)
         for k in range(len(new_representations)):
             new_state = copy(state)
             new_envs_unique = copy(state)["_envs_unique"]
