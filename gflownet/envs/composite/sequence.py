b144f46464f9b3f6eb0806ff2e79507c6b474952
Dionessa <dionessa.biton@gmail.com>
2026-08-03 18:41:16 -0400

    changed the formula for the logn correction

--- gflownet/envs/composite/sequence.py
+++ gflownet/envs/composite/sequence.py
@@ -974,7 +974,7 @@ class Sequence(CompositeBase):
         for idx, state in enumerate(states):
             # n_unique = len(self._enumerate_all_states_for_the_sequence(state))
             n_indices = len(copy(state)["_indices"])
-            if n_indices < 2: 
+            if n_indices <= 2: 
                 n_unique = 1
             else:
                 n_unique = 2**(n_indices-2)
