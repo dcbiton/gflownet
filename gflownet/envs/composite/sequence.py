6817e3b2089af46865efb60f107b4f71bb94376b
Dionessa <dionessa.biton@gmail.com>
2026-08-03 11:42:23 -0400

    Match sequence.py to create_sequence_class_dev

--- gflownet/envs/composite/sequence.py
+++ gflownet/envs/composite/sequence.py
@@ -782,7 +782,7 @@ class Sequence(CompositeBase):
 
             # here insert other variations of the 1-step-backward-state that represents the same sequence
             # merge states indicate if the states that can represent the same sequence will be enumerated
-            if ((self.merge_states) and (len(self.state["_indices"])>1)):
+            if (self.merge_states) and (len(self.state["_indices"]) > 1):
                 self.state = self._get_random_equivalent_sequence(
                     self.state, self.merge_states
                 )
@@ -970,13 +970,9 @@ class Sequence(CompositeBase):
         """
         Follows the implementation of BaseSet
         """
-        print("GET LOG PROBS OF SAME SEQUENCE", states)
         logprobs = torch.zeros(len(states), dtype=self.float, device=self.device)
         for idx, state in enumerate(states):
-            # n_unique = len(self._enumerate_all_states_for_the_sequence(state))
-            n_indices = len(copy(state)["_indices"])
-            n_unique = 2**(n_indices-1)
-            print("N UNIQUE:", n_unique, "STATE:", state)
+            n_unique = len(self._enumerate_all_states_for_the_sequence(state))
             logprobs[idx] = -torch.log(
                 tfloat(n_unique, device=self.device, float_type=self.float)
             )
@@ -1019,8 +1015,6 @@ class Sequence(CompositeBase):
                     states_stochastic = [
                         s for s, f in zip(states_from, is_eos_state) if f
                     ]
-                    print("SELF STATE:", self.state)
-                    print("STATES STOCHASTIC:", states_stochastic)
                     # log(n) correction for multiple states of the parent of the same sequences
                     # not sure yet if it is the parent that should be considered
                     logprobs[is_eos_state] += self._get_logprobs_of_same_sequences(
@@ -1334,7 +1328,7 @@ class Sequence(CompositeBase):
             return state
         else:
             # first get all the possible states
-            if len(state["_indices"]) < 2: 
+            if len(state["_indices"]) < 2:
                 return state
             all_possible_states = self._enumerate_all_states_for_the_sequence(state)
             # then choose a random state among the possible states with uniform probability
